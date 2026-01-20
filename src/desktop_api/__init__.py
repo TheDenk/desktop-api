ce483f37d9a1bce9f9ea5c366f59dedc2269eeda
TheDenk <welcomedenk@gmail.com>
2026-01-20 14:56:11 +0300

    pypi

--- src/desktop_api/__init__.py
+++ src/desktop_api/__init__.py
@@ -1,5 +1,6 @@
 """Public API for the desktop_api package."""
 from . import actions, capture, window
+from .__version__ import __version__
 from .controller import DesktopController
 from .window import WindowHandle, WindowNotFoundError
 
@@ -10,4 +11,5 @@ __all__ = [
     "actions",
     "capture",
     "window",
+  "__version__",
 ]
